646fea211e6e007846648d888ab80d44edaf2274
ben-steinberg-geffen <bsteinb53@geffenacademy.ucla.edu>
2025-02-03 09:21:57 -0800

    Add files via upload

---
+++ auto_email.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart 
+
+def email_student(student, matched_tutor):
+    message = MIMEMultipart()
+    message['From'] = 'peertutoring@geffenacademy.ucla.edu'
+    message['To'] = student.email
+
+    message['Subject'] = f'Peer Tutoring with {matched_tutor.name} on possible dates'
+    message['Body'] = f'Dear {student.name}, \n Congratulations! You have succesfully been matched 
+    with {matched_tutor.name} for peer tutoring in these classes: {student.classes}. {matched_tutor.name} is available to meet
+    with you on: {student.availability}.'
+
+def email_tutor(matched_student, tutor):
+    message = MIMEMultipart()
