2324dedebd14fdc72e5427bb6521d0354fa8cb8f
cam-235 <cdesai28@geffenacademy.ucla.edu>
2025-02-03 09:59:37 -0800

    Updated auto_email.py

--- auto_email.py
+++ auto_email.py
@@ -7,10 +7,10 @@ def email_student(student, matched_tutor):
     message['From'] = 'peertutoring@geffenacademy.ucla.edu'
     message['To'] = student.email
 
-    message['Subject'] = f'Peer Tutoring with {matched_tutor.name} on possible dates'
-    message['Body'] = f'Dear {student.name}, \n Congratulations! You have succesfully been matched 
-    with {matched_tutor.name} for peer tutoring in these classes: {student.classes}. {matched_tutor.name} is available to meet
-    with you on: {student.availability}.'
+    message['Subject'] = (f'Peer Tutoring with {matched_tutor.name} on possible dates')
+    message['Body'] = (f'Dear {student.name}, \n Congratulations! You have succesfully been matched') 
+    (f'with {matched_tutor.name} for peer tutoring in these classes: {student.classes}. {matched_tutor.name} is available to meet')
+    (f'with you on: {student.availability}.')
 
 def email_tutor(matched_student, tutor):
     message = MIMEMultipart()
