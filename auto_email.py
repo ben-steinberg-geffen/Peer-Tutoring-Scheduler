078c7b906ada892dfa5555ee73f0b3dd0f0f3e5f
themagicguardian <92544393+themagicguardian@users.noreply.github.com>
2025-02-05 14:26:37 -0800

    autoemailchange

--- auto_email.py
+++ auto_email.py
@@ -27,3 +27,20 @@ def checkavailaibility(student, tutor):
             if student.availability == tutor.availability:
                 intersection.append(student.availability, tutor.availaibility)
     return intersection
+
+
+
+import smtplib
+
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+# start TLS for security
+s.starttls()
+# Authentication
+s.login("comsciii77@gmail.com", "testpassword123")
+# message to be sent
+message = "Message_you_need_to_send"
+# sending the mail
+s.sendmail("comsciii77@gmail.com", "erobins95@geffenacademy.ucla.edu", message)
+# terminating the session
+s.quit()
