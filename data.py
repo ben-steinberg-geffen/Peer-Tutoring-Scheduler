5565cbb2aedc182ae55ab367d5145aa53b86ae7b
llhert30 <llhert30@geffenacademy.ucla.edu>
2025-02-13 11:54:22 -0800

    Fixed app-breaking bug

--- data.py
+++ data.py
@@ -1,7 +1,32 @@
 import os
 import pandas as pd
-from main import Student, Tutor
 
+class Student:
+    def __init__(self, name, email, grade, availability, courses, not_tutors, final_tutor = None):
+        self.name = name
+        self.email = email
+        self.grade = grade
+        self.availability = availability
+        self.courses = courses
+        self.matched_tutors = []
+        self.not_tutors = not_tutors
+        self.tutor_index = 0 
+        self.time_index = 0
+        self.final_tutor = final_tutor
+        self.final_time = None
+
+class Tutor:
+    def __init__(self, name, email, grade, availability, courses, not_students):
+        self.name = name
+        self.email = email
+        self.grade = grade
+        self.availability = availability
+        self.courses = courses
+        self.not_students = not_students
+        self.matched_students = []
+        self.final_students = {}
+        self.final_times = []
+        
 def load_student_data(path="student_responses.csv"):
     """
     Load student requests data from a CSV file, rename columns for consistency, and merge course selections.
