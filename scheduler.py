203abeed62605a0adee1a48cdf20728092acaff2
ben-steinberg <bsteinb53@geffenacademy.ucla.edu>
2025-03-11 12:04:39 -0700

    broken but progress

--- scheduler.py
+++ scheduler.py
@@ -21,10 +21,10 @@ def match_students_tutors(students, tutors):
 def get_not_matched(students, tutors):
     not_matched = {}
     for student in students:
+        reason = "tutors that teach your course are not available at the same time"
         if not student.matched_tutors:
             potential_times = []
             if (set(student.courses).intersection(set(tutor.courses)) == set(student.courses) and not any(set(student.availability).intersection(set(tutor.availability))) for tutor in tutors):
-                reason = "tutors that teach your course are not available at the same time"
                 for tutor in tutors: 
                     if set(student.courses).intersection(set(tutor.courses)) == set(student.courses):
                         for time in tutor.availability:
@@ -35,6 +35,13 @@ def get_not_matched(students, tutors):
             else:
                 reason = "NONE"
             not_matched[student] = [reason, potential_times]
+        if len(student.availability) < len(student.courses):
+            reason = "student needs to enter more times of availability"
+            not_matched[student] = [reason, []]
+
+        # this is the only precaution we took for this case, we need to make it so that if the availability doesn't 
+        # line up for other courses then we need to add that to the not_matched dictionary
+
     return not_matched
 
 def select_unassigned_tutor(students):
@@ -78,6 +85,13 @@ def backtrack(student_assignment, time_assignment, students, tutors):
         tutor.final_students[student] = time
         tutor.final_times.append(time)
 
+
+        for student in time_assignment.keys():
+            for student_2 in time_assignment.keys():
+                if student != student_2:        
+                    if student.name == student_2.name and time_assignment[student] == time_assignment[student_2]:
+                        print(f"Student {student.name} with course {student.courses} and {student_2.courses} are assigned to the same time {time_assignment[student]}")
+
         if check_constraints(student_assignment, time_assignment):
             result = backtrack(student_assignment, time_assignment, students, tutors)
 
@@ -103,11 +117,10 @@ def check_constraints(student_assignment, time_assignment):
                 return False
             
     for student in time_assignment.keys():
-        for student_2 in time_assignment.keys():
-            if student == student_2:
-                continue
-            if student.name == student_2.name and time_assignment[student] == time_assignment[student_2]:
-                return False
+       for student_2 in time_assignment.keys():
+           if student != student_2:        
+               if student.name == student_2.name and time_assignment[student] == time_assignment[student_2]:
+                   return False
             
     for tutor in student_assignment.values():
         if len(tutor.final_students) > 2:
