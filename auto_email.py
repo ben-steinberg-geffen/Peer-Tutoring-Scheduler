84a0be8bf070819504815a9704684399569781b2
Deracc <driover@geffenacademy.ucla.edu>
2025-04-16 09:30:04 -0700

    updated password, test email

--- auto_email.py
+++ auto_email.py
@@ -8,11 +8,11 @@ OREOSAREBADFORYOU35
 
 def auto_email(recipient, subject, message):
     from_email = 'peertutoring2@geffenacademy.ucla.edu'
-    recipient_email = recipient.email
+    recipient_email = 'driover73@geffenacademy.ucla.edu' #recipient.email
 
     text = f"Subject: {subject}\n\n{message}"
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    server.login(from_email, "OREOSAREBADFORYOU35")
+    server.login(from_email, "auam cwoo skkj epqv") #Passkey password
     server.sendmail(from_email, recipient_email, text)    
